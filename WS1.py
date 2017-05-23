acfabf5d9f24a75ab46271c8ea7cf9b31cb912e9
Mike Christofas <mchristofas@gmail.com>
2017-05-23 08:41:04 -0400

    first anemometer code try

---
+++ WS1.py
@@ -0,0 +1,35 @@
+
+# WS1.py
+# 5/21/17
+# surfstandingup@gmail.com
+
+
+
+from time import sleep
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BCM) # setup BCM GPIO numbering scheme
+
+# Setup input pin
+GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+# Callbackup function to run in another thread when button pressed
+def buttonPressed(channel):
+    global counter
+    counter += 1
+    print "Button Pressed:",counter
+
+
+
+# add event listener on pin 18
+# event will interrupt the program and call the buttonPressed function
+GPIO.add_event_detect(18, GPIO.FALLING, callback=buttonPressed, bouncetime=150)
+counter = 0
+try:
+    while True:
+        sleep(1) 
+
+
+finally:
+    GPIO.cleanup()
+    print "gpio pins are clean!"
